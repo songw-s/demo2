afa475be088c7d0c9173b7c472543fb78d8d95f5
songw-s <2604952343@qq.com>
2023-07-31 17:54:32 +0900

    emit

---
+++ components/HomeIndex.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+        <p>child: {{name}}</p>
+        <button v-on:click="changeC">changeChild</button>
+    </div>
+</template>
+<script>
+export default {
+  props: ['name'],
+  methods: {
+    changeC () {
+      this.$emit('update:name', 'newChild')
+    }
+  }
+}
+</script>
