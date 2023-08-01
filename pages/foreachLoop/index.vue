8fd8ad33628f826fd428fb602c395fa97b9820e4
songw-s <2604952343@qq.com>
2023-08-01 17:13:24 +0900

    20230801-4

---
+++ pages/foreachLoop/index.vue
@@ -0,0 +1,48 @@
+<template>
+  <div>
+    <ul>
+      <li>{{ mountaiList[0] }}</li>
+    </ul>
+    <ul>
+      <input v-model="mountaiList[1]" disabled="disabled">
+    </ul>
+    <ul>
+      <input v-bind:value="showVbind" />
+      <button v-on:click="showVbindMethod">change</button>
+    </ul>
+    <ul>
+      <input v-bind:value="foreachValueList" />
+    </ul>
+  </div>
+</template>
+<script>
+import { $axios } from '@nuxtjs/axios'
+export default {
+  data () {
+    return {
+      showVbind: 'aaa',
+      foreachValueList: []
+    }
+  },
+  async asyncData ({ app }) {
+    let mountaiList = await app.$axios.$get('https://api.open-meteo.com/v1/forecast?latitude=35.6895&longitude=139.6917&hourly=temperature_2m')
+    mountaiList = mountaiList.hourly.time
+    return { mountaiList }
+  },
+  mounted () {
+    this.FreshForeachList()
+  },
+  methods: {
+    showVbindMethod () {
+      this.showVbind = 'bbb'
+    },
+    FreshForeachList () {
+      const forValList = { 'keyword1': 'kw1', 'keyword2': 'kw2' }
+      for (let i in forValList) {
+        console.log(forValList[i])
+        this.foreachValueList.push(forValList[i])
+      }
+    }
+  }
+}
+</script>
