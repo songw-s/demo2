7ef1b75629c0f111df9f931a79f5ea707094d8a8
songw-s <2604952343@qq.com>
2023-08-01 17:13:00 +0900

    20230801-3
    追加

--- .eslintrc.js
+++ .eslintrc.js
@@ -19,6 +19,8 @@ module.exports = {
     "indent": ["error", 2],
     "eol-last": "off",
     "no-unused-vars": "off",
-    "no-multiple-empty-lines": [2, {"max": 99999, "maxEOF": 0}]
+    "no-multiple-empty-lines": [2, {"max": 99999, "maxEOF": 0}],
+    "quote-props": "off",
+    "prefer-const": "off"
   }
 }
